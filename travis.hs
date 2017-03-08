481ce066d1d946ae88821b7d5f46786fa8ea9c46
Jacob Mitchell <jmitchell@member.fsf.org>
2017-03-08 09:47:26 -0800

    Apply proposed patch to fix cpphs under GHC 7.4.

--- travis.hs
+++ travis.hs
@@ -7,5 +7,7 @@ main = do
   system_ "sudo apt-get update -y"
   system_ "sudo apt-get install -y darcs"
   system_ "darcs get http://code.haskell.org/cpphs"
+  system_ "cd cpphs && darcs apply avoid-using-findfile_-ghc-7_4-doesn_t-include-it_.dpatch"
+  system_ "pwd"
   system_ "cd cpphs && cabal update && cabal install && ghc --make cpphs -o cpphs && ./cpphs --version"
   putStrLn "Done"
