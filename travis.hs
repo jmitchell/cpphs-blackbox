4b0b4b4e390aa75de013eed46b8ef27d974c5336
Jacob Mitchell <jmitchell@member.fsf.org>
2017-03-07 21:38:18 -0800

    Use cd instead of pushd

--- travis.hs
+++ travis.hs
@@ -7,5 +7,5 @@ main = do
   system_ "sudo apt-get update -y"
   system_ "sudo apt-get install -y darcs"
   system_ "darcs get http://code.haskell.org/cpphs"
-  system_ "pushd cpphs && ghc --make cpphs -o cpphs && ./cpphs --version && popd"
+  system_ "cd cpphs && ghc --make cpphs -o cpphs && ./cpphs --version"
   putStrLn "Done"
