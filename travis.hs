d28904c2dd4275350c58e9929452d9ed4a32ea24
Jacob Mitchell <jmitchell@member.fsf.org>
2017-03-07 21:32:15 -0800

    Add travis.hs script

---
+++ travis.hs
@@ -0,0 +1,11 @@
+import Control.Exception.Extra
+import System.Time.Extra
+import System.Process.Extra
+
+main :: IO ()
+main = do
+  system_ "sudo apt-get update -y"
+  system_ "sudo apt-get install -y darcs"
+  system_ "darcs get http://code.haskell.org/cpphs"
+  system_ "pushd cpphs && ghc --make cpphs -o cpphs && ./cpphs --version && popd"
+  putStrLn "Done"
